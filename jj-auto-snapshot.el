0ee6a430a1c1143f41d67a054c895ca1a0e0fde1
Hauke Rehfeld <git@haukerehfeld.de>
2025-01-09 14:08:09 +0100

    Now draw the rest of the fucking owl.
    Signed-off-by: Hauke Rehfeld <git@haukerehfeld.de>

---
+++ jj-auto-snapshot.el
@@ -0,0 +1,55 @@
+;;https://github.com/tim-janik/jj-fzf/blob/trunk/contrib/jj-undirty.el
+(defvar jj-auto-snapshot--log-buffer-name "*jj-auto-snapshot-mode-log*"
+  "Automatically snapshot the current JJ repository on every save.")
+
+(defvar jj-auto-snapshot-snapshot-command '("jj" "--no-pager" "status" "--color=never")
+  "Command to execute to snapshot the current repository. Should be a list of strings to hand to `start-process'.")
+
+(defun jj-auto-snapshot-dominating-file ()
+  (locate-dominating-file "." ".jj"))
+
+(defun jj-auto-snapshot--take-snapshot()
+  "Execute `jj status` to snapshot the current repository.
+This function checks if the current buffer resides in a JJ repository,
+and if so executes `jj status` while logging the command output to
+the `jj-auto-snapshot--log-buffer-name' buffer.
+"
+  (interactive)
+  (when (jj-auto-snapshot-dominating-file)	; detect JJ repo
+    (progn
+      (let ((absfile (buffer-file-name))
+	          (buffer (get-buffer-create jj-auto-snapshot--log-buffer-name))
+ 	          (process-connection-type nil))	; use a pipe instead of a pty
+        (let* ((cmd (if (functionp jj-auto-snapshot-snapshot-command)
+                        (funcall jj-auto-snapshot-snapshot-command)
+                      jj-auto-snapshot-snapshot-command))
+               (cmd-str (string-join (mapcar #'shell-quote-argument cmd) " ")))
+	        (with-current-buffer buffer
+	          (goto-char (point-max))		; append to end of buffer
+	          (insert "\n# jj-auto-snapshot--take-snapshot: " absfile "\n" cmd-str "\n")
+            (apply 'start-process
+                   cmd-str
+                   buffer
+                   cmd))
+	        )))))
+
+;; no idea why this is needed, but I get undefined variable errors without it
+(defvar-local jj-auto-snapshot-mode nil "Automatically snapshot the current JJ repository on every save.")
+
+(define-minor-mode jj-auto-snapshot-mode
+  "Automatically snapshot the current JJ repository on every save."
+  :lighter " jjsnap"
+  :var jj-auto-snapshot-mode
+  (if jj-auto-snapshot-mode
+      (add-hook 'before-save-hook 'jj-auto-snapshot--take-snapshot nil 'local)
+    (remove-hook 'before-save-hook 'jj-auto-snapshot--take-snapshot 'local)))
+
+(define-minor-mode jj-auto-snapshot-global-mode
+  "Automatically snapshot the current JJ repository on every save."
+  :lighter " jjsnap"
+  :global t
+  (if jj-auto-snapshot-global-mode
+      (add-hook 'before-save-hook 'jj-auto-snapshot--take-snapshot)
+    (remove-hook 'before-save-hook 'jj-auto-snapshot--take-snapshot)))
+
+(provide 'jj-auto-snapshot)
